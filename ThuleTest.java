8cdeda65878b8c021fb0cf9f5a1f721a90c1012d
MathiasFick <mathiias-f@live.com>
2016-04-07 10:19:16 +0200

    backup

---
+++ ThuleTest.java
@@ -0,0 +1,16 @@
+package testPackage;
+
+import static org.lwjgl.glfw.GLFW.*;
+import java.nio.ByteBuffer;
+import testPackage.MoveCursor;
+
+public class ThuleTest {
+
+
+	public static void main(String[] args) {
+		KeyPress kp = new KeyPress();
+		kp.test();
+	
+	}
+
+}
